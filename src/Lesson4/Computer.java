374afe7fec766ef037dadd10ea05a03be5bff073
Nikolai-Shims <Nikolai.shims@gmail.com>
2020-06-25 21:18:31 +0300

    Lesson

--- src/Lesson4/Computer.java
+++ src/Lesson4/Computer.java
@@ -92,5 +92,8 @@ public class Computer {
             break;
         }
     }
+//public String speak(int random, int scanValue){
+  //      return random, scanValue;
+//}
 }
 
--- src/Lesson4/Computer1.java
+++ src/Lesson4/Computer1.java
@@ -1,5 +1,8 @@
 package Lesson4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Computer1 {
     public static void main(String[] args) throws Exception {
         Computer comp = new Computer("AMD", 4,250);
@@ -9,5 +12,13 @@ public class Computer1 {
         comp.characteristics();
         comp.on();
         comp.on();
+
+ //   Random r = new Random();
+   // int randomValue = r.nextInt(3);
+     //   Scanner scanner = new Scanner(System.in);
+       // int scanValue = scanner.nextInt();
+
+
+
     }
 }
